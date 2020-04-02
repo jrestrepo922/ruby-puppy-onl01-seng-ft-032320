f24022c956f7865e27bf2907dbeb9e30184c7d59
JUAN M RESTREPO <tinto922@gmail.com>
2020-04-02 12:46:21 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,27 @@
 # Add your code here
 class Dog 
   
+  attr_accessor :name
   
+  @@all = []
   
   
   def initialize(name)
     @name = name 
     @@all << self 
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @@dog_names = []
+    
+    @@all.each { |dog|
+     @@dog_names << dog.name
+    }
+    
+    puts @@dog_names
   end 
 end 
